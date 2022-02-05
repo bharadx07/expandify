ad99730626f8ebc9c9edcdc8c3490513ca78c6be
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2022-02-04 22:15:38 -0800

    revert: -1 indexing doesnt work

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const expandify = (fileName, variables) => {
   const raw = fs.readFileSync(fileName, "utf-8");
 
   const evaled = raw.replace(/{(.*)}/g, (expression) => {
-    const withoutCurly = expression.substring(1, -1);
+    const withoutCurly = expression.substring(1, expression.length - 1);
 
     return betterEval(withoutCurly, variables);
   });
